e38fc80ede286a6886c7b73f982f908c50431836
Kate-Pikachu <pikulina.ev@phystech.edu>
2021-04-01 18:30:24 +0300

    added 1 task

--- task1/main.cpp
+++ task1/main.cpp
@@ -1,40 +1,82 @@
 #include <iostream>
 #include <random>
 #include <vector>
-#include <future>
+#include <mutex>
+#include <thread>
+
+
 
 int in_circle(int n) {
+
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<> dist(0.0, 1.0);
 
     int points_in_circle = 0;
+
     for (auto i = 0; i < n; ++i) {
         double x = dist(gen);
         double y = dist(gen);
+
         if (x * x + y * y <= 1.0) {
             ++points_in_circle;
         }
+
     }
+
     return points_in_circle;
 }
 
 double r_pi_sequential(int p) {
+
     double all_points_inside = in_circle(p);
     double pi = ((double)all_points_inside / (double)p) * 4.0;
     return pi;
+
+}
+
+void Ppi(int n, double& W) {
+
+    double a, b;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dist(0.0, 1.0);
+    std::uniform_real_distribution<> dist_2(a, b);
+
+    int points_in_circle = 0;
+
+    for (auto i = 0; i < n; ++i) {
+
+        double x = dist_2(gen);
+        double y = dist(gen);
+
+        if (x * x + y * y <= 1.0) {
+
+            ++points_in_circle;
+        }
+
+    }
+
+    W += points_in_circle;
+
 }
 
 double r_pi_parallel(int p, int n) {
+
     int per_thread = p / n;
-    std::vector<std::future<int>> futures;
-    for (int t = 0; t < n; ++t) {
-        futures.emplace_back(async(std::launch::async, in_circle, per_thread));
+    std::vector<std::thread> vec(per_thread);
+    double all_points_inside = 0;
+
+    for (auto t = 0; t < n; ++t) {
+
+        vec[t] = std::thread(Ppi, (double)t / (double)n,
+            (double)t + 1 / (double)n,
+            per_thread, std::ref(all_points_inside));
+
     }
 
-    int all_points_inside = 0;
-    for (std::future<int>& f : futures) {
-        all_points_inside += f.get();
+    for (auto i = 0; i < n; ++i) {
+        vec[i].join();
     }
 
     double pi = 4.0 * (double)all_points_inside / (double)p;
